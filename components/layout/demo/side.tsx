70956c3e40eca092babe477320382047066aba95
ghoulkh <91310945+ghoulkh@users.noreply.github.com>
2024-05-20 17:32:25 +0700

    Update side.tsx
    Signed-off-by: ghoulkh <91310945+ghoulkh@users.noreply.github.com>

--- components/layout/demo/side.tsx
+++ components/layout/demo/side.tsx
@@ -33,7 +33,7 @@ const items: MenuItem[] = [
   getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
-    getItem('Alex', '5'),
+    getItem('Khanh', '5'),
   ]),
   getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
   getItem('Files', '9', <FileOutlined />),
